51616c437d663f38aab11ae81ab0982ca9a2494c
林高遠 <lky@program.com.tw>
2016-10-29 12:44:04 +0800

    初次可正常自我收發版本

---
+++ src/SerialPort.cpp
@@ -0,0 +1,90 @@
+#include "SerialPort.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+SerialPort::SerialPort(string strDevice)
+{
+	this->nFd = OpenDevice(strDevice);
+	if(-1 == this->nFd)	return;
+
+	fcntl(this->nFd, F_SETFL, 0);
+
+    struct termios options;
+    tcgetattr(this->nFd, &options);
+
+    cfsetispeed(&options, B19200);
+    cfsetospeed(&options, B19200);
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    options.c_iflag &= ~(IXON | IXOFF | IXANY);
+    options.c_oflag &= ~OPOST;
+
+    tcsetattr(this->nFd, TCSANOW, &options);
+}
+
+bool SerialPort::isOpen()
+{
+	return (-1 == this->nFd) ? false : true;
+}
+
+SerialPort::~SerialPort() {
+	// TODO Auto-generated destructor stub
+}
+
+int SerialPort::OpenDevice(string strDevice)
+{
+	int	fd = open(strDevice.c_str(), O_RDWR|O_NOCTTY|O_NDELAY );
+	if (-1 == fd)
+	{
+		perror("Can't Open Serial Port");
+		return -1;
+	}
+
+	if( (fcntl(fd, F_SETFL, 0)) < 0 )
+	{
+		perror("Fcntl F_SETFL Error!\n");
+		return -1;
+	}
+
+	if(isatty(STDIN_FILENO)==0)
+	{
+		printf("standard input is not a terminal device\n");
+		return -1;
+	}
+
+	return fd;
+}
+
+int SerialPort::GetFileDescriptor()
+{
+	return this->nFd;
+}
+
+int SerialPort::Send(string strOutMsg){
+	int nCount = write(this->nFd, strOutMsg.c_str(), strOutMsg.length());
+	return nCount;
+	
+	//printf("\nSend %d of %ld: %s\n", nCount, strOutMsg.length(), strOutMsg.c_str());
+}
+
+void SerialPort::Recv(void)
+{
+	int nread;
+	char * msg = new char[512];
+	string strRxBuf = "";
+		
+	do{
+		nread = read(this->nFd,msg,512);
+		if(0 >= nread)break;
+		
+		strRxBuf.append(msg);
+		memset(msg,0,512);//清空
+	}while(0 < nread);
+	
+	cout << strRxBuf << endl;
+}
---
+++ src/SerialPort.h
@@ -0,0 +1,32 @@
+#ifndef SERIALPORT_H_
+#define SERIALPORT_H_
+
+#include     <stdio.h> 
+#include     <stdlib.h> 
+#include     <unistd.h>  
+#include     <sys/types.h>
+#include     <sys/stat.h>
+#include     <fcntl.h> 
+#include     <termios.h> 
+#include     <errno.h>
+#include	 <string>
+using namespace std;
+
+#define BAUDRATE B19200 //Baud rate : 19200
+#define SIZE 1024
+
+class SerialPort {
+public:
+	SerialPort(string strDevice);
+	virtual ~SerialPort();
+	bool isOpen();
+	int GetFileDescriptor();
+	int Send(string strOutMsg);
+	void Recv(void);
+
+private:
+	int OpenDevice(string strDevice);
+	int nFd;
+};
+
+#endif /* SERIALPORT_H_ */
